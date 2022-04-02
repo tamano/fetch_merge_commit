e30b3e30b5d8c3a38c89f3d56a1829db8a1cc626
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2022-04-02 19:52:29 +0900

    Add function to read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let mut f = File::open(filename)
+        .expect("File not found");
+
+    let mut text = String::new();
+    f.read_to_string(&mut text)
+        .expect("File unreadable");
+
+    println!("{}", text)
 }

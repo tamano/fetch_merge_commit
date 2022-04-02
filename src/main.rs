0c257334bd9b4d961eae7ebf03a4f46ebbcb253b
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2022-04-02 20:22:32 +0900

    Change config to struct.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::env;
+use std::{env, process};
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let mut f = File::open(filename)
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error: Problem on parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    let mut f = File::open(config.filename)
         .expect("File not found");
 
     let mut text = String::new();
@@ -13,4 +17,20 @@ fn main() {
         .expect("File unreadable");
 
     println!("{}", text)
+}
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() != 2 {
+            return Err("Illegal number of  Argument");
+        }
+
+        let filename = args[1].clone();
+
+        Ok(Config { filename })
+    }
 }

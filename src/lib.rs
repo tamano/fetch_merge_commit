48963b4f4332ba30fc0058dc638086e58fc20dc0
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2022-04-02 23:35:01 +0900

    Split funtions (run -> get_merge_info -> fetch_github)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 
 pub struct Config {
     pub filename: String,
@@ -19,12 +20,34 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
+    let f = File::open(config.filename)?;
+    let reader = BufReader::new(f);
 
-    let mut text = String::new();
-    f.read_to_string(&mut text)?;
-
-    println!("{}", text);
+    for line in reader.lines() {
+        let result = get_merge_info(line?);
+        println!("{}", result);
+    }
 
     Ok(())
+}
+
+// TODO: not implemented
+fn get_merge_info(pr_url: String) -> &'static str {
+    fetch_github(pr_url)
+}
+
+// TODO: not implemented
+fn fetch_github(pr_url: String) -> &'static str {
+
+    println!("target url: {}", pr_url);
+
+    r#"
+    {
+        "name": "John Doe",
+        "age": 43,
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    }"#
 }
